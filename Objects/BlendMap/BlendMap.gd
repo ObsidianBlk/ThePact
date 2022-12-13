e576568f8b20d8b34340e9a20a1d9271d427922f
Bryan Miller <bmiller1008@gmail.com>
2022-12-12 20:04:24 -0500

    Mostly finished BlendMap node and added some examples to the current World. Created some textures and new vehicle sprite. Vehicle node now supports a 'headlight'.

--- Objects/BlendMap/BlendMap.gd
+++ Objects/BlendMap/BlendMap.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node2D
 class_name BlendMap
 
@@ -24,27 +25,39 @@ class_name BlendMap
 func set_texture_scale(s : float) -> void:
 	if s > 0.0:
 		texture_scale = s
+		print("Updating Scale")
+		_UpdateShaderParam(&"texture_scale", texture_scale)
 
 func set_texture_a(t : Texture) -> void:
 	texture_a = t
+	print("Updating TextureA")
+	_UpdateShaderParam(&"textureA", texture_a)
 
 func set_texture_b(t : Texture) -> void:
 	texture_b = t
+	print("Updating TextureB")
+	_UpdateShaderParam(&"textureB", texture_b)
 
 func set_texture_c(t : Texture) -> void:
 	texture_c = t
+	print("Updating TextureC")
+	_UpdateShaderParam(&"textureC", texture_c)
 
 func set_texture_d(t : Texture) -> void:
 	texture_d = t
+	print("Updating TextureD")
+	_UpdateShaderParam(&"textureD", texture_d)
 
 func set_blend_texture(t : Texture) -> void:
 	blend_texture = t
 	if _sprite:
+		print("Attempting to set Sprite Texture")
 		_sprite.texture = blend_texture
 
 func set_preserve_blend_alpha(e : bool) -> void:
 	if e != preserve_blend_alpha:
 		preserve_blend_alpha = e
+		_UpdateShaderParam(&"preserve_alpha", preserve_blend_alpha)
 
 # ------------------------------------------------------------------------------
 # Override Methods
@@ -52,7 +65,23 @@ func set_preserve_blend_alpha(e : bool) -> void:
 func _ready() -> void:
 	if blend_texture != null:
 		_sprite.texture = blend_texture
+	_UpdateAllShaderParams()
+
+# ------------------------------------------------------------------------------
+# Private Methods
+# ------------------------------------------------------------------------------
+func _UpdateAllShaderParams() -> void:
+	_UpdateShaderParam(&"texture_scale", texture_scale)
+	_UpdateShaderParam(&"textureA", texture_a)
+	_UpdateShaderParam(&"textureB", texture_b)
+	_UpdateShaderParam(&"textureC", texture_c)
+	_UpdateShaderParam(&"textureD", texture_d)
+	_UpdateShaderParam(&"preserve_alpha", preserve_blend_alpha)
 
 
+func _UpdateShaderParam(property : StringName, value) -> void:
+	if _sprite:
+		if _sprite.material != null:
+			_sprite.material.set_shader_parameter(property, value)
 
 
